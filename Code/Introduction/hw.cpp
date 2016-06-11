6b5ce3561b1e9de2faebc106ae54fadee4aea053
Sandeep Koranne <sandeep.koranne@gmail.com>
2016-06-11 14:47:50 -0700

    Added hello world code

---
+++ Code/Introduction/hw.cpp
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////
+// File   : hw.cpp
+// Author : Sandeep Koranne (C) 2016. All rights reserved.
+// Purpose: Introduction to Numerical Methods in Julia and C++
+//
+////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+
+int main( int argc, char* argv[] )
+{
+  std::cout << "Hello, World from " << argv[0] << std::endl;
+  return ( EXIT_SUCCESS );
+}
+
